cc40add14044e4d8d9ede23c3b9c7269bdba1926
Paola Garbato <paolagarbato@gmail.com>
2020-10-10 21:03:53 -0300

    Alterando o tema Bootstrap

--- SalesWebMVC/SalesWebMVC/Startup.cs
+++ SalesWebMVC/SalesWebMVC/Startup.cs
@@ -60,7 +60,7 @@ namespace SalesWebMVC
 
             app.UseMvc(routes =>
             {
-                routes.MapRoute(
+                routes.MapRoute( 
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
